63ada491fa91b2bad6bbad389bb490213dca58b3
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-01-28 18:53:58 +0000

    create greyscale map

--- js/map.js
+++ js/map.js
@@ -1,14 +1,34 @@
- function initialize() {
-        var mapOptions = {
-         	center: new google.maps.LatLng(51.5247969, -0.1036586),
-          	zoom: 10,
-      		zoomControl: false,
-      		panControl: false,
-      		scaleControl: false,
-      		streetViewControl: false,
-      		mapTypeControl: false
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
- 	var map = google.maps.event.addDomListener(window, 'load', initialize);
+
+window.onload = function () {  
+    var styles = [  
+    	{  
+	        featureType: 'all',  
+	        stylers: [  
+	            { saturation: -100 }  
+        	]  
+    	}
+	];  
+    var options = {  
+    mapTypeControlOptions: {  
+        mapTypeIds: ['Styled']  
+    },  
+        center: new google.maps.LatLng(51.5247969, -0.1036586),  
+        zoom: 12,  
+        mapTypeId: 'Styled',
+        mapTypeControl: false,
+        scrollwheel: false,
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.LARGE,
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        streetViewControl: false,  
+    };  
+    var div = document.getElementById('map-canvas');  
+    var map = new google.maps.Map(div, options);  
+    var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });  
+    map.mapTypes.set('Styled', styledMapType);  
+};  
