48023b32841537bff35d7b4ff730557b1e1af829
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-03-18 17:07:58 +0000

    all dropdown hover working

--- js/menu-ck.js
+++ js/menu-ck.js
@@ -1,2 +1,2 @@
 // Dropdown Menu
-$("#camera").hover(function(){$(".camera-upload").toggleClass("active")});$("#audio").hover(function(){$(".audio-upload").toggleClass("active")});
+$("#camera").hover(function(){$(".camera-upload").toggleClass("active")});$("#audio").hover(function(){$(".audio-upload").toggleClass("active")});$("#quote").hover(function(){$(".quote-upload").toggleClass("active")});$("#video").hover(function(){$(".video-upload").toggleClass("active")});
--- js/menu.js
+++ js/menu.js
@@ -8,3 +8,11 @@ $('#camera').hover(function() {
 $('#audio').hover(function() {
   $('.audio-upload').toggleClass('active');
 });
+
+$('#quote').hover(function() {
+  $('.quote-upload').toggleClass('active');
+});
+
+$('#video').hover(function() {
+  $('.video-upload').toggleClass('active');
+});
