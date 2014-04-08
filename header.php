f41437eb17cccdb5f6ee51899be2d05a4895d9d7
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-04-08 12:16:27 +0100

    add seperate mobile map styles

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Our City LDN</title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDi60LYLiOyTBn3mtcxf3d8n5JN8uzhXf8&sensor=true"></script>
 
         <link rel="stylesheet" href="stylesheets/style.css">
--- js/map.js
+++ js/map.js
@@ -1,34 +1,65 @@
 
-window.onload = function () {  
-    var styles = [  
-    	{  
-	        featureType: 'all',  
-	        stylers: [  
-	            { saturation: -100 }  
-        	]  
-    	}
-	];  
-    var options = {  
-    mapTypeControlOptions: {  
-        mapTypeIds: ['Styled']  
-    },  
-        center: new google.maps.LatLng(51.5247969, -0.1036586),  
-        zoom: 14,  
-        mapTypeId: 'Styled',
-        mapTypeControl: false,
-        scrollwheel: false,
-        panControl: true,
-        panControlOptions: {
-            position: google.maps.ControlPosition.RIGHT_CENTER
+window.onload = function () {
+    var useragent = navigator.userAgent;
+    var mapdiv = document.getElementById("map-canvas");
+
+    if (useragent.indexOf('iPhone') !== -1 || useragent.indexOf('Android') !== -1 ) {
+        styles = [
+            {
+                featureType: 'all',
+                stylers: [
+                    { saturation: -100 }
+                ]
+            }
+        ];
+        options = {
+        mapTypeControlOptions: {
+            mapTypeIds: ['Styled']
         },
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.LARGE,
-            position: google.maps.ControlPosition.RIGHT_CENTER
+            center: new google.maps.LatLng(51.5247969, -0.1036586),
+            zoom: 14,
+            mapTypeId: 'Styled',
+            zoomControl: false,
+            mapTypeControl: false,
+            scrollwheel: false,
+            panControl: false,
+            streetViewControl: false,
+        };
+
+
+    } else {
+        styles = [
+            {
+                featureType: 'all',
+                stylers: [
+                    { saturation: -100 }
+                ]
+            }
+        ];
+        options = {
+        mapTypeControlOptions: {
+            mapTypeIds: ['Styled']
         },
-        streetViewControl: false,  
-    };  
-    var div = document.getElementById('map-canvas');  
-    var map = new google.maps.Map(div, options);  
-    var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });  
-    map.mapTypes.set('Styled', styledMapType);  
-};  
+            center: new google.maps.LatLng(51.5247969, -0.1036586),
+            zoom: 14,
+            mapTypeId: 'Styled',
+            mapTypeControl: false,
+            scrollwheel: false,
+            panControl: true,
+            panControlOptions: {
+                position: google.maps.ControlPosition.RIGHT_CENTER
+            },
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.LARGE,
+                position: google.maps.ControlPosition.RIGHT_CENTER
+            },
+            streetViewControl: false,
+        };
+
+    }
+
+    var div = document.getElementById('map-canvas');
+    var map = new google.maps.Map(div, options);
+    var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });
+    map.mapTypes.set('Styled', styledMapType);
+};
