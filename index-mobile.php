11616ca2486387df868e3ff9cdc86132c76a73d6
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-04-08 12:24:48 +0100

    creade seperate index page for mobile

---
+++ index-mobile.php
@@ -0,0 +1,13 @@
+<?php include 'header.php' ?>
+    <body>
+        <div class="container">
+            <div id="header">
+                
+            </div>
+        </div>
+
+        <div id="map-canvas"></div>
+
+        <?php include 'footer.php' ?>
+    </body>
+</html>
