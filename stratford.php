8b219e71aa7fc5779656bbfcc30a0684a93a03aa
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-05-07 16:43:37 +0100

    update columbia road text

--- stratford.php
+++ stratford.php
@@ -8,4 +8,3 @@
 <div id="middleRight"><img height="228px" width="300px" src="img/stratford/strat2_thumb.jpg"></div>
 <div id="bottomLeft"><img height="228px" width="299px" src="img/stratford/strat3_thumb.jpg"></div>
 <div id="bottomRight"><img height="228px" width="300px" src="img/stratford/strat4_thumb.jpg"></div>
-<div id="text"><p>"It’s still the avant-garde theatre that started it all in a way, and here it is standing as it was in 1960 looking down towards the kings road where it all started, swinging London and all that."</p></div>
