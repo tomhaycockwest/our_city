0521947ae47e7652a90f0a3141863054da1d852e
Tom Haycock-West <tomhaycockwest@gmail.com>
2014-05-14 13:46:37 +0100

    add favicon

--- header.php
+++ header.php
@@ -8,6 +8,6 @@
         <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDi60LYLiOyTBn3mtcxf3d8n5JN8uzhXf8&sensor=true"></script>
-
+        <link rel="shortcut icon" href="img/favicon.ico">
         <link rel="stylesheet" href="stylesheets/style.css">
     </head>
